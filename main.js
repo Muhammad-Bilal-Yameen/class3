2124166c49837b01a22bc32b0c916101034f3259
muhammad bilal yameen <bilalyameen03151190023@gmail.com>
2024-01-04 16:20:18 -0800

    class3

---
+++ main.js
@@ -0,0 +1,74 @@
+//Exercise # 1
+// let userName:any
+// userName = "Muhammad"
+// userName=2222
+// console.log(userName)
+//Exercise # 2
+// let userName:string = "syed"
+// console.log(userName)
+//Exercise # 3
+// let userName:string = "sir mustafa"
+// userName="sir gulam mustafa"
+// console.log(userName)
+// console.log("talking decision")
+//Exercise # 4
+//if else
+// syntex
+// if(){}
+// else{}
+// let firstName:string = "bilal"
+// let lastNmae:string = "yameen"
+// if(firstName=="bilal"  ){
+//     console.log("well come home")
+// }
+// else{
+//     console.log("bye")
+// }
+// let budget = 40000
+// let cycle = 45000
+// if(budget < cycle){
+//     console.log("i do not affored to purchase cycle")
+// }
+// else{console.log("i can affored to puchase cycle")}
+// single = assign
+// let num:any = "3"
+// let userName = "bilal"
+// // console.log(num) // 3  
+// double mached ==
+// let a:number = 123
+// // let b:string = "bilal"
+// console.log(a ==13 ) // false
+// let car:string = "Bmw"
+// let carColor:string = "white"
+// if(carColor  = "red"){
+//     console.log(" color is not red")
+// }
+// else{
+//     console.log("color is availble")  // color is not availble
+// }
+// let car:string = "civic"
+// let carColor:string = "red"
+// console.log(carColor +"  "+ car)
+// let budge = 40000
+// let cycle = 42000
+// let color = "red"
+// if(budge > cycle  ){
+//     console.log("bye cycle")
+// }
+// else{console.log("not cycle")} // not cycle
+// let raita:string = "biryani"
+// let biscuit:string = "chocolate"
+// if( raita == "qorma" ){
+//     console.log("ly ao")
+// }
+// else{
+//     console.log("nahi lana")
+// }
+var salary = 12000;
+var monthlyExpece = 12000;
+if (salary == monthlyExpece) {
+    console.log("you have money");
+}
+else {
+    console.log("you have not money");
+}
